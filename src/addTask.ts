b7a40f0fa2bfd9b64cc532dd92d0e9e8591f7095
Nathan Arthur <nathan@nathanarthur.com>
2025-06-06 14:51:16 -0400

    return task from addTask

--- src/addTask.ts
+++ src/addTask.ts
@@ -6,13 +6,24 @@ type Input = {
   cents: number;
 };
 
+interface Task {
+  id: string;
+  task: string;
+  due: number;
+  cents: number;
+  complete: boolean;
+  status: "pending" | "complete" | "expired";
+  chargeStatus?: "notified" | "authorized" | "captured";
+  contested?: boolean;
+}
+
 // Requires that user be authenticated.
-export async function addTask(input: Input): Promise<Response> {
+export async function addTask(input: Input): Promise<Task> {
   const response = await fetch2("me/tasks", true, "POST", input);
 
   if (!response.ok) {
     throw new Error("Failed to add task");
   }
 
-  return response;
+  return response.json();
 }
